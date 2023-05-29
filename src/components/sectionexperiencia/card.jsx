37cfa7caa28430deaf1dba8d4e14106d774f96fc
Bambu Team |  Luis Enrique Concuá <development@bambu.team>
2023-05-29 16:43:06 -0600

    Center_Card

--- src/components/sectionexperiencia/card.jsx
+++ src/components/sectionexperiencia/card.jsx
@@ -6,7 +6,7 @@ import shape  from '../../assets/img/shapes/shape.png'
 const Card = () => {
   return (
     <Container>
-        <Row lg={6}>
+        <Row lg={6} className='gap Cnt_card' >
         <Col>
     <div className="card">
       <div className="circle"><h1>+ 2</h1></div>
@@ -72,18 +72,7 @@ const Card = () => {
     </div>
     </ Col>
 
-    <Col>
-    <div className="card">
-      <div className="circle"><h1>+ 2</h1></div>
-      <div className='shape'>
-      <img src={shape} alt="Shape" />
-      </div>
-      <h2>Proveedores de Infraestructura</h2>
-      <div className="hidden-content">
-        <p>Lorem Ipsum es simplemente el texto de relleno de las imprentas y archivos de texto. </p>
-      </div>
-    </div>
-    </ Col>
+   
 
     </Row>
     
