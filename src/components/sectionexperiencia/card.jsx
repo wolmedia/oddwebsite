60cdba3697da9ea97d6506a0a2ac39a6ab064234
Bambu Team |  Luis Enrique Concuá <development@bambu.team>
2023-05-29 16:13:07 -0600

    Card
    Grid 6

--- src/components/sectionexperiencia/card.jsx
+++ src/components/sectionexperiencia/card.jsx
@@ -1,17 +1,93 @@
 import React from 'react';
-import { Container } from 'react-bootstrap';
+import { Container, Col, Row } from 'react-bootstrap';
+import shape  from '../../assets/img/shapes/shape.png'
 
 
 const Card = () => {
   return (
     <Container>
+        <Row lg={6}>
+        <Col>
     <div className="card">
       <div className="circle"><h1>+ 2</h1></div>
+      <div className='shape'>
+      <img src={shape} alt="Shape" />
+      </div>
       <h2>Proveedores de Infraestructura</h2>
       <div className="hidden-content">
-        <p>Contenido oculto</p>
+        <p>Lorem Ipsum es simplemente el texto de relleno de las imprentas y archivos de texto. </p>
       </div>
     </div>
+    </ Col>
+
+    <Col>
+    <div className="card">
+      <div className="circle"><h1>+ 2</h1></div>
+      <div className='shape'>
+      <img src={shape} alt="Shape" />
+      </div>
+      <h2>Proveedores de Infraestructura</h2>
+      <div className="hidden-content">
+        <p>Lorem Ipsum es simplemente el texto de relleno de las imprentas y archivos de texto. </p>
+      </div>
+    </div>
+    </ Col>
+
+    <Col>
+    <div className="card">
+      <div className="circle"><h1>+ 2</h1></div>
+      <div className='shape'>
+      <img src={shape} alt="Shape" />
+      </div>
+      <h2>Proveedores de Infraestructura</h2>
+      <div className="hidden-content">
+        <p>Lorem Ipsum es simplemente el texto de relleno de las imprentas y archivos de texto. </p>
+      </div>
+    </div>
+    </ Col>
+
+    <Col>
+    <div className="card">
+      <div className="circle"><h1>+ 2</h1></div>
+      <div className='shape'>
+      <img src={shape} alt="Shape" />
+      </div>
+      <h2>Proveedores de Infraestructura</h2>
+      <div className="hidden-content">
+        <p>Lorem Ipsum es simplemente el texto de relleno de las imprentas y archivos de texto. </p>
+      </div>
+    </div>
+    </ Col>
+
+    <Col>
+    <div className="card">
+      <div className="circle"><h1>+ 2</h1></div>
+      <div className='shape'>
+      <img src={shape} alt="Shape" />
+      </div>
+      <h2>Proveedores de Infraestructura</h2>
+      <div className="hidden-content">
+        <p>Lorem Ipsum es simplemente el texto de relleno de las imprentas y archivos de texto. </p>
+      </div>
+    </div>
+    </ Col>
+
+    <Col>
+    <div className="card">
+      <div className="circle"><h1>+ 2</h1></div>
+      <div className='shape'>
+      <img src={shape} alt="Shape" />
+      </div>
+      <h2>Proveedores de Infraestructura</h2>
+      <div className="hidden-content">
+        <p>Lorem Ipsum es simplemente el texto de relleno de las imprentas y archivos de texto. </p>
+      </div>
+    </div>
+    </ Col>
+
+    </Row>
+    
+    
     </Container>
   );
 }
