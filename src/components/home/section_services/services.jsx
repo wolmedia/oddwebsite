d09af41b4b0df412ba37d31e97bf72390b9c33ee
Bambu Team |  Luis Enrique Concuá <development@bambu.team>
2023-05-30 16:00:54 -0600

    Carousel Services

--- src/components/home/section_services/services.jsx
+++ src/components/home/section_services/services.jsx
@@ -1,75 +1,114 @@
-import React, { useRef, useState } from 'react';
+import React, { useState } from 'react';
+import imagen1 from '../../../assets/img/figure/development_black.png';
+import imagen2 from '../../../assets/img/figure/seo_black.png';
+import imagen3 from '../../../assets/img/figure/tagging_black.png';
+import imagen4 from '../../../assets/img/figure/it_black.png';
+import imagen5 from '../../../assets/img/figure/digital_black.png';
+import imagen6 from '../../../assets/img/figure/atl_black.png';
 
 
-const ServicesContainer = () => {
-  const servicesData = [
+
+
+
+
+
+
+
+const Services = () => {
+
+
+
+  const [currentIndex, setCurrentIndex] = useState(0);
+  const data = [
     {
-      image: 'service1.jpg',
-      title: 'Servicio 1',
-      description: 'Descripción del Servicio 1',
-      url: 'https://ejemplo.com/servicio1',
+
+      id: 1,
+      image: imagen1,
+      title: 'DEVELOPMENT',
+      paragraph: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry.',
+      link: 'https://ejemplo.com'
     },
     {
-      image: 'service2.jpg',
-      title: 'Servicio 2',
-      description: 'Descripción del Servicio 2',
-      url: 'https://ejemplo.com/servicio2',
+      id: 2,
+      image: imagen2,
+      title: 'SEO',
+      paragraph: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry.',
+      link: 'https://ejemplo.com'
     },
     {
-      image: 'service3.jpg',
-      title: 'Servicio 3',
-      description: 'Descripción del Servicio 3',
-      url: 'https://ejemplo.com/servicio3',
+      id: 3,
+      image: imagen3,
+      title: 'TAGGING',
+      paragraph: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry.',
+      link: 'https://ejemplo.com'
     },
-    // Agrega más servicios aquí
+    {
+      id: 4,
+      image: imagen4,
+      title: 'IT',
+      paragraph: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry.',
+      link: 'https://ejemplo.com'
+    },
+    {
+      id: 5,
+      image: imagen5,
+      title: 'REPORTING DIGITAL',
+      paragraph: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry.',
+      link: 'https://ejemplo.com'
+    },
+    {
+      id: 6,
+      image: imagen6,
+      title: 'REPORTING ATL',
+      paragraph: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry.',
+      link: 'https://ejemplo.com'
+    }
+    
+  
+ 
   ];
 
-  const servicesContainerRef = useRef(null);
-  const [startX, setStartX] = useState(0);
-  const [scrollLeft, setScrollLeft] = useState(0);
-
-  const handleTouchStart = (e) => {
-    setStartX(e.touches[0].clientX);
-    setScrollLeft(servicesContainerRef.current.scrollLeft);
+  const handlePrev = () => {
+    setCurrentIndex((prevIndex) =>
+      prevIndex === 0 ? data.length - 1 : prevIndex - 1
+    );
   };
 
-  const handleTouchMove = (e) => {
-    const x = e.touches[0].clientX;
-    const walk = (x - startX) * 3; // Controla la velocidad del deslizamiento
-    servicesContainerRef.current.scrollLeft = scrollLeft - walk;
+  const handleNext = () => {
+    setCurrentIndex((prevIndex) =>
+      prevIndex === data.length - 1 ? prevIndex : prevIndex + 1
+    );
   };
+
+  const visibleCards = data.slice(currentIndex, currentIndex + 3);
+  const showNextButton = currentIndex < data.length - 3;
 
   return (
-    <div
-      className="services-container"
-      onTouchStart={handleTouchStart}
-      onTouchMove={handleTouchMove}
-      onTouchEnd={() => {
-        setStartX(0);
-        setScrollLeft(0);
-      }}
-    >
-      <h2>Servicios Principales</h2>
-      <div className="services" ref={servicesContainerRef}>
-        {servicesData.map((service, index) => (
-          <div className="service" key={index}>
-            <img src={service.image} alt={service.title} className="service-image" />
-            <h3 className="service-title">{service.title}</h3>
-            <p className="service-description">{service.description}</p>
-            <a href={service.url} className="service-link">
-              Ver más
-            </a>
+
+    <div className="carousel">
+      <div className="card-container">
+        {visibleCards.map((card) => (
+          <div className="card" key={card.id}>
+            <img src={card.image} alt={card.title} />
+            <h2>{card.title}</h2>
+            <p>{card.paragraph}</p>
+            <a href={card.link}>Ir</a>
           </div>
         ))}
       </div>
-      <button className="scroll-button left" onClick={() => servicesContainerRef.current.scrollLeft -= 200}>
-        <i className="fas fa-chevron-left"></i>
-      </button>
-      <button className="scroll-button right" onClick={() => servicesContainerRef.current.scrollLeft += 200}>
-        <i className="fas fa-chevron-right"></i>
-      </button>
+      <div className="navigation">
+        <button onClick={handlePrev} disabled={currentIndex === 0}>
+          Anterior
+        </button>
+        {showNextButton && (
+          <button onClick={handleNext}>
+            Siguiente
+          </button>
+        )}
+      </div>
     </div>
   );
 };
 
-export default ServicesContainer;
+export default Services;
+
