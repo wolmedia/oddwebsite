3dad3c9d6253d14fa430caaf733bd2f7ecc8de52
Bambu Team |  Luis Enrique Concuá <75746976+BambuTeam@users.noreply.github.com>
2023-06-06 17:01:24 -0600

    Card2
    Card2

--- src/components/home/section_services/services.jsx
+++ src/components/home/section_services/services.jsx
@@ -1,3 +1,5 @@
+import Image1 from '../../../assets/img/figure/development_black.png';
+
 
 import React, { useState } from 'react';
 
@@ -5,10 +7,10 @@ const Carousel = () => {
   const carouselData = [
     {
       id: 1,
-      imageUrl: '',
+      imageUrl: {Image1},
       title: 'Título 1',
       description: 'Descripción de la tarjeta 1',
-      buttonUrl: 'https://www.ejemplo.com/1',
+      buttonUrl:  'https://www.ejemplo.com/2',
     },
     {
       id: 2,
@@ -20,54 +22,49 @@ const Carousel = () => {
     {
       id: 3,
       imageUrl: 'ruta-de-la-imagen-2.jpg',
-      title: 'Título 2',
+      title: 'Título 3',
       description: 'Descripción de la tarjeta 2',
       buttonUrl: 'https://www.ejemplo.com/2',
     },
     {
       id: 4,
       imageUrl: 'ruta-de-la-imagen-2.jpg',
-      title: 'Título 2',
+      title: 'Título 4',
       description: 'Descripción de la tarjeta 2',
       buttonUrl: 'https://www.ejemplo.com/2',
     },
     {
       id: 5,
       imageUrl: 'ruta-de-la-imagen-2.jpg',
-      title: 'Título 2',
+      title: 'Título 5',
       description: 'Descripción de la tarjeta 2',
       buttonUrl: 'https://www.ejemplo.com/2',
     },
     {
-      id: 3,
+      id: 6,
       imageUrl: 'ruta-de-la-imagen-2.jpg',
-      title: 'Título 2',
+      title: 'Título 6',
       description: 'Descripción de la tarjeta 2',
       buttonUrl: 'https://www.ejemplo.com/2',
     }
   ];
 
-
-
-  const [activeIndex, setActiveIndex] = useState(0);
+  const [startIndex, setStartIndex] = useState(0);
 
   const handlePrev = () => {
-    setActiveIndex((prevIndex) => (prevIndex === 0 ? carouselData.length - 1 : prevIndex - 1));
+    setStartIndex((prevIndex) => (prevIndex === 0 ? carouselData.length - 3 : prevIndex - 1));
   };
 
   const handleNext = () => {
-    setActiveIndex((prevIndex) => (prevIndex === carouselData.length - 1 ? 0 : prevIndex + 1));
+    setStartIndex((prevIndex) => (prevIndex === carouselData.length - 3 ? 0 : prevIndex + 1));
   };
 
   return (
     <div className="carousel">
       <button onClick={handlePrev}>Anterior</button>
       <div className="card-container">
-        {carouselData.map((card, index) => (
-          <div
-            key={card.id}
-            className={`card ${index === activeIndex ? 'active' : ''}`}
-          >
+        {carouselData.slice(startIndex, startIndex + 3).map((card) => (
+          <div key={card.id} className="card">
             <img src={card.imageUrl} alt="Imagen principal" />
             <h2>{card.title}</h2>
             <p>{card.description}</p>
