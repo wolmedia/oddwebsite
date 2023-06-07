7543fecc62d0dcf4061b3d84f696b3c70c3388b1
Bambu Team |  Luis Enrique Concuá <development@bambu.team>
2023-06-07 16:03:02 -0600

    Test2

--- src/components/home/section_services/services.jsx
+++ src/components/home/section_services/services.jsx
@@ -1,4 +1,8 @@
 import React, { useState } from 'react';
+import { Container, Row, Col } from 'react-bootstrap';
+import { Carousel } from 'react-responsive-carousel';
+import 'react-responsive-carousel/lib/styles/carousel.min.css';
+import { BsChevronLeft, BsChevronRight } from 'react-icons/bs';
 import imagen1 from '../../../assets/img/figure/development_black.png';
 import gif1 from '../../../assets/img/Gif/tria 2.gif';
 import imagen2 from '../../../assets/img/figure/seo_black.png';
@@ -7,18 +11,11 @@ import imagen3 from '../../../assets/img/figure/tagging_black.png';
 import imagen4 from '../../../assets/img/figure/it_black.png';
 import imagen5 from '../../../assets/img/figure/digital_black.png';
 import imagen6 from '../../../assets/img/figure/atl_black.png';
-import { Container, Row,Col } from 'react-bootstrap';
-
-
 
 const Services = () => {
-
-
-
   const [currentIndex, setCurrentIndex] = useState(0);
   const data = [
     {
-
       id: 1,
       image: imagen1,
       gif: gif1,
@@ -62,9 +59,6 @@ const Services = () => {
       paragraph: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry.',
       link: 'https://ejemplo.com'
     }
-
-
-
   ];
 
   const handlePrev = () => {
@@ -85,46 +79,50 @@ const Services = () => {
   return (
     <Container>
       <Row className="justify-content-md-center">
-        
-        <div className="carousel">
-        <Col>
-          <div className="card-container">
-
-            {visibleCards.map((card) => (
-              <div className="card" key={card.id}>
-                <img src={card.image} alt={card.title} />
-                <h2>{card.title}</h2>
-                <p>{card.paragraph}</p>
-                <a href={card.link}>Ir</a>
-              </div>
-            ))}
-
-          </div>
+        <div className="carousel_solution">
+          <Col>
+            <Carousel
+              showStatus={false}
+              showThumbs={false}
+              showArrows={false}
+              emulateTouch
+              swipeable
+            >
+              {visibleCards.map((card) => (
+                <div className="boxes" key={card.id}>
+                  <img src={card.image} alt={card.title} />
+                  <h2>{card.title}</h2>
+                  <p>{card.paragraph}</p>
+                  <a href={card.link}>Ir</a>
+                </div>
+              ))}
+            </Carousel>
           </Col>
-          <div className="navigation">
-             <Col>
-            <button onClick={handlePrev} disabled={currentIndex === 0}>
-              <p></p>
-            </button>
-            </Col> 
-
-            {showNextButton && (
-
-              <Col>
-              <button onClick={handleNext}>
-              <p></p>
-
+          <div className="navigation_arrow">
+            <Col>
+              <button
+                className="carousel-navigation-btn"
+                onClick={handlePrev}
+                disabled={currentIndex === 0}
+              >
+                <BsChevronLeft />
               </button>
+            </Col>
+            {showNextButton && (
+              <Col>
+                <button
+                  className="carousel-navigation-btn"
+                  onClick={handleNext}
+                >
+                  <BsChevronRight />
+                </button>
               </Col>
-
             )}
           </div>
         </div>
-
       </Row>
     </Container>
   );
 };
 
 export default Services;
-
