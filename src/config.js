c999bd6fb51b2850943c099ac28789dd83c69452
Bambu Team |  Luis Enrique Concuá <development@bambu.team>
2023-06-26 12:29:49 -0600

    Up

--- src/config.js
+++ src/config.js
@@ -1,7 +1,7 @@
 // in config.js
 import { createChatBotMessage } from 'react-chatbot-kit';
 
-const botName = 'ExcitementBot';
+const botName = 'ODD 🤖';
 
 const config = {
   initialMessages: [createChatBotMessage(`Hi! I'm ${botName}`)],
