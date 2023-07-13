c6382bd077736addf5d68ef50f5f4e39c3881e68
Bambu Team |  Luis Enrique Concuá <lconcua.odd@gmail.com>
2023-07-13 17:11:31 -0600

    Hover

--- src/components/sectionexperiencia/card.jsx
+++ src/components/sectionexperiencia/card.jsx
@@ -17,7 +17,7 @@ const Card = () => {
       <div className='shape'>
       <img src={shape} alt="Shape" />
       </div>
-      <h2 >Infraestructura</h2>
+      <h2 >Servidores cloud</h2>
       <div className="hidden-content">
         <p>Impulsamos la innovación protegiendo datos e información con nuestra sólida infraestructura en la nube. </p>
       </div>
@@ -27,13 +27,13 @@ const Card = () => {
 
     <Col>
     <div className="card">
-      <div className="circle"><h1>+50</h1></div>
+      <div className="circle"><h1>+30</h1></div>
       <div className='shape'>
       <img src={shape} alt="Shape" />
       </div>
-      <h2>Desarrollos Web</h2>
+      <h2>Clientes</h2>
       <div className="hidden-content">
-        <p>Lorem Ipsum es simplemente el texto de relleno de las imprentas y archivos de texto. </p>
+        <p>Nuestro éxito se basa en la confianza de nuestros clientes para satisfacer sus necesidades tecnológicas. </p>
       </div>
     </div>
     </ Col>
@@ -44,9 +44,22 @@ const Card = () => {
       <div className='shape'>
       <img src={shape} alt="Shape" />
       </div>
-      <h2>Servidores de DB</h2>
+      <h2>Automatizaciones</h2>
       <div className="hidden-content">
-        <p>Lorem Ipsum es simplemente el texto de relleno de las imprentas y archivos de texto. </p>
+        <p>Optimizamos y automatizamos procesos empresariales, permitiendo agilizar operaciones y mejor aprovechamiento de tiempo en cualquier giro de negocio</p>
+      </div>
+    </div>
+    </ Col>
+
+    <Col>
+    <div className="card">
+      <div className="circle"><h1>+50</h1></div>
+      <div className='shape'>
+      <img src={shape} alt="Shape" />
+      </div>
+      <h2>Páginas Web</h2>
+      <div className="hidden-content">
+        <p> Diseñamos experiencias web atractivas y funcionales que cautivan a usuarios generando resultados efectivos.</p>
       </div>
     </div>
     </ Col>
@@ -59,20 +72,7 @@ const Card = () => {
       </div>
       <h2>Soluciones</h2>
       <div className="hidden-content">
-        <p>Lorem Ipsum es simplemente el texto de relleno de las imprentas y archivos de texto. </p>
-      </div>
-    </div>
-    </ Col>
-
-    <Col>
-    <div className="card">
-      <div className="circle"><h1>+25</h1></div>
-      <div className='shape'>
-      <img src={shape} alt="Shape" />
-      </div>
-      <h2>Automatizaciones</h2>
-      <div className="hidden-content">
-        <p>Lorem Ipsum es simplemente el texto de relleno de las imprentas y archivos de texto. </p>
+        <p>Nuestra experiencia nos permite desarrollar soluciones tecnológicas personalizadas que impulsan negocios hacia el éxito.</p>
       </div>
     </div>
     </ Col>
