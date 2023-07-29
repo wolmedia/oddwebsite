30bd048d01e0ecf7481118b55bbc62ed7ab88d3d
Bambu Team |  Luis Enrique Concuá <lconcua.odd@gmail.com>
2023-07-28 19:58:54 -0600

    Solution_cards

--- src/components/Development/SolucionesDev.jsx
+++ src/components/Development/SolucionesDev.jsx
@@ -1,5 +1,21 @@
 import Container from 'react-bootstrap/Container';
 import {Row, Col} from 'react-bootstrap';
+import Crm from '../../assets/img/Dev/crm.webp'
+import Website from '../../assets/img/Dev/website.webp'
+import Landing from '../../assets/img/Dev/landingpage.webp'
+import Ecommerce from '../../assets/img/Dev/ecommerce.webp'
+import ML from '../../assets/img/Dev/machinelearning.webp'
+import Ar from '../../assets/img/Dev/ar.webp'
+import Webmaster from '../../assets/img/Dev/webmaster.webp'
+import Apps from '../../assets/img/Dev/webmaster.webp'
+
+
+
+
+
+
+
+
 
 
 
@@ -20,6 +36,51 @@ function Solucionesdev(){
         Lorem Ipsum is simply dummy text of the printing and typesetting industry. 
         </p>
       </Row>
+
+    <Row lg={6} className=' Cnt_solutions' >
+        <Col>
+        <img src={Crm} alt="Circulo" />
+        </ Col>
+        <Col>
+        <img src={Website} alt="Circulo" />
+        </ Col>
+        <Col>
+        <img src={Landing} alt="Circulo" />
+        </ Col>
+        <Col>
+        <img src={Ecommerce} alt="Circulo" />
+        </ Col>
+    </Row>
+
+    <Row lg={6} className=' Cnt_solutions' >
+        <Col>
+        <img src={ML} alt="Circulo" />
+        </ Col>
+        <Col>
+        <img src={Ar} alt="Circulo" />
+        </ Col>
+        <Col>
+        <img src={Webmaster} alt="Circulo" />
+        </ Col>
+        <Col>
+        <img src={Apps} alt="Circulo" />
+        </ Col>
+    </Row>
+
+    <Row lg={6} className=' Cnt_solutions' >
+        <Col>
+        <img src={Crm} alt="Circulo" />
+        </ Col>
+        <Col>
+        <img src={Website} alt="Circulo" />
+        </ Col>
+        <Col>
+        <img src={Landing} alt="Circulo" />
+        </ Col>
+        <Col>
+        <img src={Ecommerce} alt="Circulo" />
+        </ Col>
+    </Row>
       </Container>
 
 
