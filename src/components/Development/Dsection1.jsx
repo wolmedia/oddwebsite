077db5f45794b79d1a402a31a13cc03bd9612c9d
Bambu Team |  Luis Enrique Concuá <lconcua.odd@gmail.com>
2023-07-28 19:05:09 -0600

    Dev_section1

--- src/components/Development/Dsection1.jsx
+++ src/components/Development/Dsection1.jsx
@@ -1,4 +1,7 @@
 import Container from 'react-bootstrap/Container';
+import {Row, Col} from 'react-bootstrap';
+import ImageDev from '../../assets/img/Dev/img.png'
+
 
 
 
@@ -6,14 +9,30 @@ function Banner_Dev (){
     return(
 
     <Container fluid className='Section1_Dev' >
+      <Row>
       <div>
         <div>
         <p>
-        “Creamos soluciones tecnológicas a medida, desde páginas web hasta inteligencia artificial, para llevar a cabo tu negocio al siguiente nivel”
+        <strong>“Creamos soluciones tecnológicas a medida</strong>, desde páginas web hasta inteligencia artificial, para llevar a cabo tu negocio al siguiente nivel”
         </p>
         
         </div>
       </div>
+      </Row>
+      
+      <Row>
+        <Col className='img_Dev'>
+        <img src={ImageDev} alt="Circulo" />
+        
+        </Col>
+        <Col>
+        <h1>titular</h1>
+        <p className='Dev_p2'>
+        Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.
+        </p>
+        </Col>
+
+      </Row>
       </Container>
 
 
