8e69fd8a54f7cd0b30f66f2b16ea439126a4b8d6
Luis Carlos Estrada <111467024+lestradaWOL@users.noreply.github.com>
2023-09-07 10:29:05 -0600

    Navbar-update

--- src/components/navbar/navbar.jsx
+++ src/components/navbar/navbar.jsx
@@ -7,41 +7,88 @@ import NavDropdown from "react-bootstrap/NavDropdown";
 
 function Navbar() {
   return (
-    <NavbarGeneral collapseOnSelect expand="lg" className="bg-body-tertiary">
-      <Container>
-        <NavbarGeneral.Brand href="#home">
+    <>
+      <Nav className="Navbar_odd desktop-flex">
+        <div className="navbar-logo">
           <a href="/">
-            <img  className="logo-navbar" src={LogoNavgar} alt="Logo" />
+            <img src={LogoNavgar} alt="Logo" />
           </a>
-        </NavbarGeneral.Brand>
-        <NavbarGeneral.Toggle aria-controls="responsive-navbar-nav" />
-        <NavbarGeneral.Collapse id="responsive-navbar-nav">
-          <Nav className="ml-auto">
-            <Nav.Link href="#features">Quienes Somos</Nav.Link>
-            <Nav.Link href="#pricing">Experiencia</Nav.Link>
-            <NavDropdown title="Soluciones" id="collasible-nav-dropdown" className="navbar-language">
-              <NavDropdown.Item href="/development">
-                development
-              </NavDropdown.Item>
-              <NavDropdown.Item href="/seo">seo</NavDropdown.Item>
-              <NavDropdown.Item href="/tagging">tagging</NavDropdown.Item>
-              <NavDropdown.Item href="/atl">atl</NavDropdown.Item>
-              <NavDropdown.Item href="/data-digital">
-                data digital
-              </NavDropdown.Item>
-              <NavDropdown.Item href="/informatica">ti</NavDropdown.Item>
-            </NavDropdown>
-            <Nav.Link href="#contact">Contacto</Nav.Link>
-          </Nav>
-          <Nav>
-            <NavDropdown title="español" className="navbar-language">
-              <NavDropdown.Item value="en">English</NavDropdown.Item>
-              <NavDropdown.Item value="es">Español</NavDropdown.Item>
-            </NavDropdown>
-          </Nav>
-        </NavbarGeneral.Collapse>
-      </Container>
-    </NavbarGeneral>
+        </div>
+
+        <ul className="navbar-links">
+          <li>
+            <a href="/">Quienes somos</a>
+          </li>
+          <li>
+            <a href="/">Experiencia</a>
+          </li>
+          {/* <li><a href="/Development">Soluciones</a>
+        
+        </li> */}
+          <NavDropdown title="soluciones" className="navbar-language">
+            <NavDropdown.Item href="/development">
+              {" "}
+              development
+            </NavDropdown.Item>
+            <NavDropdown.Item href="/seo">seo</NavDropdown.Item>
+            <NavDropdown.Item href="/tagging">tagging</NavDropdown.Item>
+            <NavDropdown.Item href="/atl">atl</NavDropdown.Item>
+            <NavDropdown.Item href="/data-digital">
+              data digital
+            </NavDropdown.Item>
+            <NavDropdown.Item href="/informatica">ti</NavDropdown.Item>
+          </NavDropdown>
+          <li>
+            <a href="/">Contactos</a>
+          </li>
+        </ul>
+
+        <NavDropdown title="español" className="navbar-language">
+          <NavDropdown.Item value="en">English</NavDropdown.Item>
+          <NavDropdown.Item value="es">Español</NavDropdown.Item>
+        </NavDropdown>
+      </Nav>
+
+      <NavbarGeneral collapseOnSelect expand="lg" className="bg-body-tertiary mobile">
+        <Container>
+          <NavbarGeneral.Brand href="#home">
+            <a href="/">
+              <img className="logo-navbar" src={LogoNavgar} alt="Logo" />
+            </a>
+          </NavbarGeneral.Brand>
+          <NavbarGeneral.Toggle aria-controls="responsive-navbar-nav" />
+          <NavbarGeneral.Collapse id="responsive-navbar-nav">
+            <Nav className="ml-auto">
+              <Nav.Link href="#features">Quienes Somos</Nav.Link>
+              <Nav.Link href="#pricing">Experiencia</Nav.Link>
+              <NavDropdown
+                title="Soluciones"
+                id="collasible-nav-dropdown"
+                className="navbar-language"
+              >
+                <NavDropdown.Item href="/development">
+                  development
+                </NavDropdown.Item>
+                <NavDropdown.Item href="/seo">seo</NavDropdown.Item>
+                <NavDropdown.Item href="/tagging">tagging</NavDropdown.Item>
+                <NavDropdown.Item href="/atl">atl</NavDropdown.Item>
+                <NavDropdown.Item href="/data-digital">
+                  data digital
+                </NavDropdown.Item>
+                <NavDropdown.Item href="/informatica">ti</NavDropdown.Item>
+              </NavDropdown>
+              <Nav.Link href="#contact">Contacto</Nav.Link>
+            </Nav>
+            <Nav>
+              <NavDropdown title="español" className="navbar-language">
+                <NavDropdown.Item value="en">English</NavDropdown.Item>
+                <NavDropdown.Item value="es">Español</NavDropdown.Item>
+              </NavDropdown>
+            </Nav>
+          </NavbarGeneral.Collapse>
+        </Container>
+      </NavbarGeneral>
+    </>
   );
 }
 
